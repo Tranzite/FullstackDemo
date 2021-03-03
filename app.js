88be6bbadbd7e503ac2ba6857401cfb1a49310b0
NiklasHj <54146045+NiklasHj@users.noreply.github.com>
2021-03-03 09:00:15 +0100

    updated server to handle post requests.

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const staticDir = __dirname + "\\static\\";
 
-app.get("/", (req, res) => res.send("Niklas for President!"));
+app.use(express.static(staticDir));
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get("/", (req, res) => res.sendFile(staticDir + "html\\index.html"));
+
+app.get("/contact", (req, res) =>
+  res.sendFile(staticDir + "html\\contact.html")
+);
+
+app.post("/contact", function (req, res) {
+  console.log(req.body.fname);
+  console.log(req.body.date);
+  res.redirect("/");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
